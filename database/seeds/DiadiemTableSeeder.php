b8bdc13e64aa44f0adf3ec4f6834f1ad059ae9d1
Nguyen <trinhthenguyen123@gmail.com>
2019-04-11 20:21:14 +0700

    update seed

--- database/seeds/DiadiemTableSeeder.php
+++ database/seeds/DiadiemTableSeeder.php
@@ -12,9 +12,9 @@ class DiadiemTableSeeder extends Seeder
     public function run()
     {
         DB::table('diadiem')->insert([
-        	['id'=>'LT1', 'ten'=>'Phòng Lý Thuyết 1','tinhtrang'=>1],
-        	['id'=>'LT4', 'ten'=>'Phòng Lý Thuyết 2','tinhtrang'=>0],
-        	['id'=>'LT5', 'ten'=>'Phòng Lý Thuyết 3','tinhtrang'=>0]
+        	['id'=>'LT1', 'ten'=>'Phòng Lý Thuyết 1'],
+        	['id'=>'LT4', 'ten'=>'Phòng Lý Thuyết 2'],
+        	['id'=>'LT5', 'ten'=>'Phòng Lý Thuyết 3']
         ]);
     }
 }
